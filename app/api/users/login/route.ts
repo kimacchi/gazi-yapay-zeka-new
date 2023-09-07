9743dff4893d344750a30dc1d87757c44c490047
Kimacchi <shiftusu2@gmail.com>
2023-09-07 20:25:04 +0300

    wip

---
+++ app/api/users/login/route.ts
@@ -0,0 +1,15 @@
+import { test } from "@/controllers/newsController"
+import { login } from "@/controllers/userController"
+import { User } from "@/types/user"
+import { NextApiRequest } from "next"
+
+interface UserRequest extends NextApiRequest{
+    json(): Promise<User>
+    body: User
+}
+
+export async function POST(request: UserRequest) {
+    const body = await request.json()
+    const authData = await login(body.email, body.password)
+    return new Response(JSON.stringify(authData))
+}
