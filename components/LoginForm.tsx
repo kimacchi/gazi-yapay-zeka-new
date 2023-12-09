154e2b6f10f124baae826d9149d571051fc6888c
Kimacchi <shiftusu2@gmail.com>
2023-12-09 21:10:09 +0300

    forgot password done

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,6 +1,15 @@
 "use client";
 
-import { Spinner } from "@nextui-org/react";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  Spinner,
+  useDisclosure,
+} from "@nextui-org/react";
 import axios from "axios";
 import Link from "next/link";
 import React, { useState } from "react";
@@ -59,51 +68,104 @@ const LoginForm = () => {
     }
   };
 
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [resetEmail, setResetEmail] = useState("");
+  const resetPassword = async () => {
+    try {
+      const resetResponse = await axios.post<{
+        error?: any;
+        status?: number;
+      }>("/api/users/password_reset", {
+        email: resetEmail.trim(),
+      });
+      if (resetResponse.data.error) {
+        console.log("mail not sent.");
+        alert("Bir hata oluştu. Lütfen tekrar deneyiniz.");
+      } else {
+        console.log("mail sent.");
+        alert("Şifre sıfırlama bağlantısı e-posta adresinize gönderildi.")
+      }
+      setResetEmail("");
+    } catch (error) {
+      throw error;
+    }
+  }
+
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-      {
-        error && (
+    <>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="center">
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Şifrenizi mi unuttunuz?
+              </ModalHeader>
+              <ModalBody>
+                <p>
+                  Endişeye gerek yok, aşağıdaki alana şifresini unuttuğunuz
+                  hesabın e-posta adresini girip ve e-postanıza gelen bağlantıyı
+                  kullanarak şifrenizi sıfırlayabilirsiniz.
+                </p>
+                <input
+                  type="email"
+                  className="sm:w-96 w-72 h-10 px-2 rounded-md bg-transparent border-2 border-white"
+                  value={resetEmail}
+                  placeholder="E-posta adresiniz"
+                  onChange={(event) => setResetEmail(event.target.value)}
+                />
+              </ModalBody>
+              <ModalFooter className="flex flex-col items-center">
+                <Button color="primary" onPress={resetPassword}>
+                  Şifremi sıfırla
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+        {error && (
           <h2 className="sm:w-96 w-72 text-base p-2 rounded-lg bg-rose-700/50">
             Bilgiler geçersiz. Tekrar deneyiniz.
             <br></br>
             <span className="text-xs">
-            Aksi durumda idari kurul ile iletişime geçiniz.
+              Aksi durumda idari kurul ile iletişime geçiniz.
             </span>
           </h2>
-        )
-      }
-      <div className="flex flex-col gap-6">
-        <input
-          type="email"
-          className="sm:w-96 w-72 h-10 px-2 rounded-md bg-transparent border-2 border-white"
-          value={email}
-          placeholder="E-posta adresiniz..."
-          onChange={(event) => setEmail(event.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Şifreniz..."
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-          className="sm:w-96 w-72 h-10 px-2 rounded-md bg-transparent border-2 border-white"
-        />
-      </div>
-      <div className="flex w-full justify-between">
-        <Link href="/signup" className="hover:underline">
-          Kayıt ol
-        </Link>
-        <Link href="/" className="hover:underline">
-          Şifremi unuttum
-        </Link>
-      </div>
-      <button
-        type="submit"
-        disabled={loading}
-        className="text-xl sm:w-96 mt-2 w-72 border-4 font-extrabold p-4 tracking-widest rounded-md border-white transition-all hover:bg-white hover:text-neutral-900"
-      >
-        {loading ? <Spinner /> : "Giriş yap"}
-      </button>
-    </form>
+        )}
+        <div className="flex flex-col gap-6">
+          <input
+            type="email"
+            className="sm:w-96 w-72 h-10 px-2 rounded-md bg-transparent border-2 border-white"
+            value={email}
+            placeholder="E-posta adresiniz..."
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Şifreniz..."
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            className="sm:w-96 w-72 h-10 px-2 rounded-md bg-transparent border-2 border-white"
+          />
+        </div>
+        <div className="flex w-full justify-between">
+          <Link href="/signup" className="hover:underline">
+            Kayıt ol
+          </Link>
+          <button className="hover:underline" type="button" onClick={onOpen}>
+            Şifremi unuttum
+          </button>
+        </div>
+        <button
+          type="submit"
+          disabled={loading}
+          className="text-xl sm:w-96 mt-2 w-72 border-4 font-extrabold p-4 tracking-widest rounded-md border-white transition-all hover:bg-white hover:text-neutral-900"
+        >
+          {loading ? <Spinner /> : "Giriş yap"}
+        </button>
+      </form>
+    </>
   );
 };
 
