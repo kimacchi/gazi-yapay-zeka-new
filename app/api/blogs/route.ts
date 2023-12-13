6c4b82423d77921902cc66b04b48c6e84b8dc8b5
Kimacchi <shiftusu2@gmail.com>
2023-12-13 21:54:13 +0300

    wip

--- app/api/blogs/route.ts
+++ app/api/blogs/route.ts
@@ -7,7 +7,7 @@ const pb = new PocketBase("https://gazi-yapay-zeka.pockethost.io");
 export async function GET(req: Request) {
     const token = cookies().get("pb_auth")?.value
     pb.authStore.loadFromCookie(token || "");
-    const blogs = await getAllBlogs()
+    const blogs = await getAllBlogs(pb)
     return new Response(JSON.stringify(blogs))
 }
 
@@ -15,6 +15,6 @@ export async function POST(req: Request) {
     const token = cookies().get("pb_auth")?.value
     pb.authStore.loadFromCookie(token || "");
     const body = await req.formData()
-    const record = await createBlog(body)
+    const record = await createBlog(body,pb)
     return new Response(JSON.stringify(record))
 }
