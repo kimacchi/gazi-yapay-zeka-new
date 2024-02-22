8750157b241e732527f0a5b22eff2553f6e6efa8
Ahmet Ergül <shiftusu2@gmail.com>
2024-02-22 09:54:39 +0300

    wip, cant continue in office, must do at home

--- app/dashboard/sponsors/page.tsx
+++ app/dashboard/sponsors/page.tsx
@@ -1,15 +1,29 @@
-import Link from 'next/link'
-import React from 'react'
+import { Member } from "@/types/member";
+import axios, { AxiosResponse } from "axios";
+import { cookies, headers } from "next/headers";
+import Link from "next/link";
+import React from "react";
 
-const page = () => {
+const Page = async () => {
+  console.log(headers().get("host"))
+  const host =
+    headers().get("host") === "localhost:3000"
+      ? "http://localhost:3000"
+      : `https://${headers().get("host")}`;
+  const pb_auth = cookies().get("pb_auth")?.value;
+  // const res = await axios.get<any, AxiosResponse<Member[]>>(
+  //   `${host}/api/members`,
+  //   {
+  //     headers: {
+  //       cookie: `pb_auth=${pb_auth}`,
+  //     },
+  //   }
+  // );
   return (
     <div>
-        <Link href="/dashboard/sponsors/create">
-          Create Sponsor
-        </Link>
+      <Link href="/dashboard/sponsors/create">Create Sponsor</Link>
     </div>
+  );
+};
 
-  )
-}
-
-export default page
+export default Page;
