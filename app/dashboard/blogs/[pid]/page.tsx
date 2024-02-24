1718160d0e0c9d0e90722f6a3d215372183a6a30
Kimacchi <ahmetergul.kima@gmail.com>
2024-02-24 15:11:19 +0300

    changed the host

--- app/dashboard/blogs/[pid]/page.tsx
+++ app/dashboard/blogs/[pid]/page.tsx
@@ -6,13 +6,13 @@ import React from 'react'
 
 const Page = async ({ params }: { params: { pid: string } }) => {
   console.log(headers().get("host"));
-    const host = "http://localhost:3000";
+    // const host = "http://localhost:3000";
 
-//   const host =
-//     headers().get("host") === "localhost:3000" ||
-//     headers().get("host") !== "gaziyapayzeka.com"
-//       ? "http://localhost:3000"
-//       : `https://${headers().get("host")}`;
+  const host =
+    headers().get("host") === "localhost:3000" ||
+    headers().get("host") !== "gaziyapayzeka.com"
+      ? "http://localhost:3000"
+      : `https://${headers().get("host")}`;
 
   const pb_auth = cookies().get("pb_auth")?.value;
   const res = await axios.get<any, AxiosResponse<Blog>>(
